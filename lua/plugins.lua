b78c49e307eba442c3a5f9f6b3c787b11197211f
Rodrigo Navarro <rnavarro1@gmail.com>
2021-12-08 10:58:46 -0300

    Automatically recompile Packer when plugins.lua is changed.

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,5 +1,12 @@
 vim.cmd [[packadd packer.nvim]]
 
+vim.cmd([[
+  augroup packer_user_config
+    autocmd!
+    autocmd BufWritePost plugins.lua source <afile> | PackerCompile
+  augroup end
+]])
+
 return require('packer').startup(function()
   use 'wbthomason/packer.nvim'
 
