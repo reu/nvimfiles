77d38fd62f49d54f87e07fcf6eeac04139caa40d
Rodrigo Navarro <rnavarro1@gmail.com>
2021-12-17 12:10:15 -0300

    Adds ESLint code actions.

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -36,6 +36,7 @@ end
 local null_ls = require("null-ls")
 null_ls.config({
   sources = {
+    null_ls.builtins.code_actions.eslint,
     null_ls.builtins.diagnostics.eslint,
     null_ls.builtins.formatting.prettier,
     null_ls.builtins.formatting.stylua,
