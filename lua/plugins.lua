6e2c63bd501a16b50e04f85e3513fc51ec1cabe7
Rodrigo Navarro <rnavarro1@gmail.com>
2021-12-26 11:55:33 -0300

    Adds treesitter context.

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -60,6 +60,13 @@ return require("packer").startup(function()
   })
 
   use({ "nvim-treesitter/nvim-treesitter", run = ":TSUpdate" })
+  use({
+    "romgrk/nvim-treesitter-context",
+    requires = { "nvim-treesitter/nvim-treesitter" },
+    config = function()
+      require("treesitter-context").setup()
+    end,
+  })
 
   use({
     "nvim-telescope/telescope.nvim",
