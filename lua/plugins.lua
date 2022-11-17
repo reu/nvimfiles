2c2e8e2c02470fbf629013c3dd135237285efc3e
Rodrigo Navarro <rnavarro1@gmail.com>
2022-11-17 09:40:03 -0300

    Bootstraping packer.

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,4 +1,15 @@
-vim.cmd([[packadd packer.nvim]])
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    vim.cmd [[packadd packer.nvim]]
+    return true
+  end
+  return false
+end
+
+local packer_bootstrap = ensure_packer()
 
 vim.cmd([[
   augroup packer_user_config
@@ -100,4 +111,8 @@ return require("packer").startup(function()
       require("Comment").setup()
     end,
   })
+
+  if packer_bootstrap then
+    require('packer').sync()
+  end
 end)
