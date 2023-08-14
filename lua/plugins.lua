4205a25735077e979f1188407e840bb60d922a3d
Rodrigo Navarro <rnavarro1@gmail.com>
2023-08-13 22:31:34 -0300

    Updates packer bootstraping.

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,9 +1,9 @@
 local ensure_packer = function()
   local fn = vim.fn
-  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+  local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
   if fn.empty(fn.glob(install_path)) > 0 then
-    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
-    vim.cmd [[packadd packer.nvim]]
+    fn.system({ "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path })
+    vim.cmd([[packadd packer.nvim]])
     return true
   end
   return false
@@ -122,6 +122,6 @@ return require("packer").startup(function()
   })
 
   if packer_bootstrap then
-    require('packer').sync()
+    require("packer").sync()
   end
 end)
