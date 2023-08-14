ba8173199b9745d008307ebdd1060f23ba587817
Rodrigo Navarro <rnavarro1@gmail.com>
2023-08-13 22:34:04 -0300

    Enable incremental selection via Treesitter.

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -35,4 +35,13 @@ require("nvim-treesitter.configs").setup({
   autotag = {
     enable = true,
   },
+  incremental_selection = {
+    enable = true,
+    keymaps = {
+      init_selection = "gnn", -- set to `false` to disable one of the mappings
+      node_incremental = "grn",
+      scope_incremental = "grc",
+      node_decremental = "grm",
+    },
+  },
 })
